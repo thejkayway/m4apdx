78c714d1a00b4e9dad28b887ddf97e45d03440cb
Jonathan Kay <bellyjeans55@gmail.com>
2021-06-06 18:35:46 -0700

    Add events templates

---
+++ src/templates/event.js
@@ -0,0 +1,99 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import Layout from '../components/Layout'
+import Container from '../components/Container'
+import Hero from '../components/Hero'
+import PageBody from '../components/PageBody'
+import TagList from '../components/TagList'
+import PostLinks from '../components/PostLinks'
+import PostDetails from '../components/PostDetails'
+import SEO from '../components/SEO'
+
+const EventTemplate = ({ data, pageContext }) => {
+  const {
+    title,
+    metaDescription,
+    heroImage,
+    description,
+    eventStartDate,
+    eventUrl,
+    tags,
+  } = data.contentfulEvent
+
+  const previous = pageContext.prev
+  const next = pageContext.next
+  const { basePath } = pageContext
+
+  let ogImage
+  try {
+    ogImage = heroImage.ogimg.src
+  } catch (error) {
+    ogImage = null
+  }
+
+  return (
+    <Layout>
+      <SEO
+        title={title}
+        description={
+          metaDescription
+            ? metaDescription.internal.content
+            : description.childMarkdownRemark.excerpt
+        }
+        image={ogImage}
+      />
+      <Hero title={title} image={heroImage} height={'50vh'} />
+      <Container>
+        {tags && <TagList tags={tags} basePath={basePath} />}
+        <PostDetails
+          date={eventStartDate}
+          url={eventUrl}
+          timeToRead={description.childMarkdownRemark.timeToRead}
+        />
+        <PageBody body={description} />
+      </Container>
+      <PostLinks previous={previous} next={next} basePath={basePath} />
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query($slug: String!) {
+    contentfulEvent(slug: { eq: $slug }) {
+      title
+      slug
+      metaDescription {
+        internal {
+          content
+        }
+      }
+      eventStartDate(formatString: "MMMM DD, YYYY h:mmA")
+      eventStartDateISO: eventStartDate(formatString: "YYYY-MM-DD")
+      eventEndDate(formatString: "h:mmA")
+      eventUrl
+      heroImage {
+        title
+        fluid(maxWidth: 1800) {
+          ...GatsbyContentfulFluid_withWebp_noBase64
+        }
+        ogimg: resize(width: 1800) {
+          src
+        }
+      }
+      tags {
+        title
+        id
+        slug
+      }
+      description {
+        childMarkdownRemark {
+          timeToRead
+          html
+          excerpt(pruneLength: 320)
+        }
+      }
+    }
+  }
+`
+
+export default EventTemplate
---
+++ src/templates/events.js
@@ -0,0 +1,89 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import Layout from '../components/Layout'
+import CardList from '../components/CardList'
+import Card from '../components/Card'
+import Container from '../components/Container'
+import Pagination from '../components/Pagination'
+import SEO from '../components/SEO'
+import { startCase } from 'lodash'
+
+const Events = ({ data, pageContext }) => {
+  const events = data.allContentfulEvent.edges
+  const { humanPageNumber, basePath } = pageContext
+  const isFirstPage = humanPageNumber === 1
+  let featuredEvent
+  let ogImage
+
+  try {
+    featuredEvent = events[0].node
+  } catch (error) {
+    featuredEvent = null
+  }
+  try {
+    ogImage = events[0].node.heroImage.ogimg.src
+  } catch (error) {
+    ogImage = null
+  }
+
+  return (
+    <Layout>
+      <SEO title={startCase(basePath)} image={ogImage} />
+      <Container>
+        {isFirstPage ? (
+          <CardList>
+            <Card {...featuredEvent} featured basePath={basePath} />
+            {events.slice(1).map(({ node: event }) => (
+              <Card key={event.id} {...event} basePath={basePath} />
+            ))}
+          </CardList>
+        ) : (
+          <CardList>
+            {events.map(({ node: event }) => (
+              <Card key={event.id} {...event} basePath={basePath} />
+            ))}
+          </CardList>
+        )}
+      </Container>
+      <Pagination context={pageContext} />
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query($skip: Int!, $limit: Int!) {
+    allContentfulEvent(
+      sort: { fields: [eventStartDate], order: DESC }
+      limit: $limit
+      skip: $skip
+    ) {
+      edges {
+        node {
+          title
+          id
+          slug
+          heroImage {
+            title
+            fluid(maxWidth: 1800) {
+              ...GatsbyContentfulFluid_withWebp_noBase64
+            }
+            ogimg: resize(width: 1800) {
+              src
+            }
+          }
+          eventStartDate(formatString: "MMMM DD, YYYY h:mmA")
+          eventEndDate(formatString: "h:mmA")
+          eventUrl
+          description {
+            childMarkdownRemark {
+              html
+              excerpt(pruneLength: 80)
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+export default Events
